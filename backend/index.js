c086888b1662bc66972147cab44d47c3ec447f22
BonBon <per_cherry@163.com>
2023-03-27 22:36:15 +0800

    change backend running port

--- backend/index.js
+++ backend/index.js
@@ -40,5 +40,5 @@ app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(3000);
-console.log("Server is running on 3000");
+app.listen(3001);
+console.log("Server is running on 3001");
