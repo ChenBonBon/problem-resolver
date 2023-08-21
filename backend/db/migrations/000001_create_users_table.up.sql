96a6f61af70528058319474d3a6c40c81fdb3874
BonBon Chen <peng.chen@formal-tech.com>
2023-08-21 15:10:24 +0800

    update migrate sqls

--- backend/db/migrations/000001_create_users_table.up.sql
+++ backend/db/migrations/000001_create_users_table.up.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS users(
    password VARCHAR (64),
    status VARCHAR (16) NOT NULL DEFAULT 'Enabled',
    source VARCHAR (16) NOT NULL,
-   created_at TIMESTAMPTZ NOT NULL
+   created_at TIMESTAMPTZ NOT NULL,
    created_by VARCHAR (32) NOT NULL,
-   updated_at TIMESTAMPTZ
+   updated_at TIMESTAMPTZ,
    updated_by VARCHAR (32)
 );
--- backend/db/migrations/000003_create_problems_table.up.sql
+++ backend/db/migrations/000003_create_problems_table.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS problems(
    id SERIAL PRIMARY KEY,
    name VARCHAR (64) NOT NULL,
    description TEXT,
-   created_at TIMESTAMPTZ NOT NULL
+   created_at TIMESTAMPTZ NOT NULL,
    created_by VARCHAR (32) NOT NULL,
-   updated_at TIMESTAMPTZ
+   updated_at TIMESTAMPTZ,
    updated_by VARCHAR (32)
 );
