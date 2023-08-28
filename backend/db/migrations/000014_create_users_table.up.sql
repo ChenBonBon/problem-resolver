d4d25b94b381d0eac690af856c6b8495bfa4c278
BonBon Chen <peng.chen@formal-tech.com>
2023-08-28 14:20:34 +0800

    Rewrite 000014 migration

--- backend/db/migrations/000014_create_users_table.up.sql
+++ backend/db/migrations/000014_create_users_table.up.sql
@@ -1,2 +1,2 @@
 TRUNCATE TABLE users RESTART IDENTITY CASCADE;
-INSERT INTO users (name, email, password, source) VALUES ('per_cherry', 'per_cherry@163.com', md5('problem_resolver'), 'Password');
+INSERT INTO users (name, email, password, status, source) VALUES ('per_cherry', 'per_cherry@163.com', md5('problem_resolver'), 'Enabled', 'Password');
