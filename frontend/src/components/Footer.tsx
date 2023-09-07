658347c2d62ef0af42ed8c77c1b2addf3a854795
BonBon Chen <peng.chen@formal-tech.com>
2023-09-07 13:25:46 +0800

    ✨ feat(both):
    ## [0.0.8] - 2023-09-07
    
    ### Changed
    
    - 更新 Footer 样式

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,21 +2,19 @@ import { Link } from "@radix-ui/themes";
 import { styled } from "styled-components";
 
 const Wrapper = styled.footer`
-  position: absolute;
-  bottom: 0;
-  text-align: center;
-  padding: 16px 0;
-  width: 100%;
-  background-color: white;
-  z-index: 1;
+    text-align: center;
+    padding: 16px 0;
+    width: 100%;
+    background-color: white;
+    z-index: 1;
 `;
 
 export default function Footer() {
-  return (
-    <Wrapper>
-      <Link href="https://beian.miit.gov.cn/" target="_blank">
-        {import.meta.env.VITE_COPYRIGHT}
-      </Link>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Link href="https://beian.miit.gov.cn/" target="_blank">
+                {import.meta.env.VITE_COPYRIGHT}
+            </Link>
+        </Wrapper>
+    );
 }
